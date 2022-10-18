d6b75579e7f284020841cd488b2e9df99c832f6b
KonstChurakaev <111491525+KonstChurakaev@users.noreply.github.com>
2022-10-18 18:37:29 +0500

    v - 4

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,10 +1,8 @@
 package koschei.models;
 
-import org.springframework.beans.factory.annotation.Autowired;
 
 public class Egg6 {
 
-    @Autowired
     private Needle7 needle;
 
     public Egg6(Needle7 needle) {
